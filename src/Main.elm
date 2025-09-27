a2e1b5b51e6211859501857c2ee60a83ea1ef486
LautiB89 <tatibagnasco@gmail.com>
2025-09-27 19:05:24 -0300

    add style

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,11 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div, input, text, button)
+import Html exposing (Html, button, div, h2, h3, text, textarea)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 import LambdaParser exposing (parse, viewExpr)
-import Html.Events exposing (onClick)
+
 
 
 -- MAIN
@@ -34,7 +34,8 @@ init =
 
 
 type Msg
-    = Change String | ToggleImplicitParens
+    = Change String
+    | ToggleImplicitParens
 
 
 update : Msg -> Model -> Model
@@ -42,6 +43,7 @@ update msg model =
     case msg of
         Change newContent ->
             { model | content = newContent }
+
         ToggleImplicitParens ->
             { model | showImplicitParens = not model.showImplicitParens }
 
@@ -52,8 +54,47 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ input [ placeholder "Text to parse", value model.content, onInput Change ] []
-        , button [ onClick ToggleImplicitParens ] [ text "toggle implicit parens" ]
-        , div [] [ text (viewExpr model.showImplicitParens (parse model.content)) ]
+    let
+        ifShowParens a b =
+            if model.showImplicitParens then
+                a
+
+            else
+                b
+    in
+    div [ style "max-width" "600px", style "margin" "40px auto", style "font-family" "sans-serif", style "font-size" "24px" ]
+        [ h2 [] [ text "λ-Calculus parser" ]
+        , textarea
+            [ value model.content
+            , onInput Change
+            , placeholder "(\\x.x x) (\\y.y y)"
+            , rows 4
+            , cols 60
+            , style "margin-bottom" "12px"
+            ]
+            []
+        , div [ style "margin-bottom" "12px" ]
+            [ button
+                [ onClick ToggleImplicitParens
+                , style "background"
+                    (ifShowParens "#007acc" "#eee")
+                , style "color"
+                    (ifShowParens "white" "black")
+                , style "border" "none"
+                , style "padding" "8px 16px"
+                , style "cursor" "pointer"
+                ]
+                [ text
+                    (ifShowParens "Hide" "Show" ++ " implicit parens")
+                ]
+            ]
+        , h3 [] [ text "Parsed output:" ]
+        , div
+            [ style "background" "#f9f9f9"
+            , style "padding" "12px"
+            , style "border-radius" "4px"
+            , style "min-height" "40px"
+            , style "font-family" "monospace"
+            ]
+            [ text (viewExpr model.showImplicitParens (parse model.content)) ]
         ]
